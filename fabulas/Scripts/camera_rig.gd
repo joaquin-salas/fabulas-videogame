01ff8cf53a49a698a972f9e8395dac1edff6e743
joaquin-salas <joaquin.salas.dev@gmail.com>
2026-04-07 22:29:43 +0200

    feat(camera): basic following camera on the Y axis

--- fabulas/Scripts/camera_rig.gd
+++ fabulas/Scripts/camera_rig.gd
@@ -1,11 +1,30 @@
 extends Node2D
 
+## Camera that follows a specific node with a set of rules.
 
-# Called when the node enters the scene tree for the first time.
+# ====================== REFERENCE VARIABLES ======================
+@onready var camera: Camera2D = $Camera2D
+@export var follow_node: Node2D 
+
+@export var follow_y: bool = true
+@export var follow_x: bool = false
+@export var follow_speed: float = 6.0
+
+@export var y_offset: float = -100.0
+@export var x_offset: float = 0.0
+
+@export var zoom_value: Vector2 = Vector2(1.0, 1.0)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	camera.zoom = zoom_value
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if follow_node == null:
+		print("follow_node es null")
+		return
+	
+	if follow_x:
+		global_position.x = lerp(global_position.x, follow_node.global_position.x + x_offset, follow_speed * delta)
+	
+	if follow_y:
+		global_position.y = lerp(global_position.y, follow_node.global_position.y + y_offset, follow_speed * delta)
