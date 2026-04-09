abde2c40d6b1b4b8473795b7f097357a608cd672
isaacvc53 <isaacvc42@gmail.com>
2026-04-09 13:09:25 +0200

    fix(aerial_enemy): fix movement

--- fabulas/aerial_enemy.gd
+++ fabulas/aerial_enemy.gd
@@ -4,31 +4,40 @@ extends CharacterBody2D
 @export var WayPoints: Array[Marker2D]
 @onready var animationPajaro=$AnimationPlayer
 @onready var sprite2D=$Sprite2D
+@export var wait_time := 2.0
+
 
 var current_index = 0
 var is_waiting= false
+var target : Marker2D
+
+func _ready():
+	target = WayPoints[current_index]
 
 func _physics_process(delta):
 	if is_waiting:
 		animation(Vector2.ZERO)
 		return 
 	var min_distance = 5.0
-	var target_position = WayPoints[current_index].global_position
-	var direction = target_position - global_position
+	var direction = target.global_position - global_position
 	var distance = direction.length()
 	direction = direction.normalized()
-	velocity = direction * speed
+	velocity = velocity.lerp(direction * speed, 0.05)
 		
 	move_and_slide()
 	animation(direction)
 	
-	if distance < min_distance:
+	if distance <= min_distance:
 		current_index += 1
-		velocity = Vector2.ZERO
-		$Timer.start()
-		is_waiting = true
+	
 		if current_index >= WayPoints.size():
 			current_index = 0
+	
+		target = WayPoints[current_index]  
+	
+		velocity = Vector2.ZERO
+		$Timer.start(wait_time)
+		is_waiting = true
 	
 	if direction.x > 0:
 		sprite2D.flip_h = false
