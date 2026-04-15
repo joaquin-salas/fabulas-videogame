081a15e444b8dbb8f5f9dd096dd43cb7984d7c84
isaacvc53 <isaacvc42@gmail.com>
2026-04-15 17:43:13 +0200

    feat(map) : new scene
    nueva escena para el map y  mejorar en el enemigo

--- fabulas/aerial_enemy.gd
+++ fabulas/aerial_enemy.gd
@@ -1,48 +1,81 @@
 extends CharacterBody2D
 
 @export var speed : float
-@export var WayPoints: Array[Marker2D]
 @onready var animationPajaro=$AnimationPlayer
 @onready var sprite2D=$Sprite2D
+@onready var ground_point = $GroundPoint
 @export var wait_time := 2.0
+@export var route: Routes
+@onready var agent = $NavigationAgent2D
+
+enum Routes {
+	ROUTE_A,
+	ROUTE_B,
+	ROUTE_C
+}
 
 
+var WayPoints: Array = []
 var current_index = 0
 var is_waiting= false
 var target : Marker2D
 
+
 func _ready():
+	var group_name = ""
+
+	match route:
+		Routes.ROUTE_A:
+			group_name = "Route_A"
+		Routes.ROUTE_B:
+			group_name = "Route_B"
+		Routes.ROUTE_C:
+			group_name = "Route_C"
+	WayPoints = get_tree().get_nodes_in_group(group_name)
 	target = WayPoints[current_index]
+	agent.target_position = target.global_position
+	agent.target_desired_distance = 5.0
+	
+	
+	
 
 func _physics_process(delta):
 	if is_waiting:
 		animation(Vector2.ZERO)
-		return 
-	var min_distance = 5.0
-	var direction = target.global_position - global_position
-	var distance = direction.length()
-	direction = direction.normalized()
-	velocity = velocity.lerp(direction * speed, 0.05)
-		
+		return
+
+	var next_point = agent.get_next_path_position()
+	var direction = (next_point - global_position).normalized()
+
+	velocity = velocity.lerp(direction * speed, 0.5)
 	move_and_slide()
+
 	animation(direction)
-	
-	if distance <= min_distance:
-		current_index += 1
-	
-		if current_index >= WayPoints.size():
-			current_index = 0
-	
-		target = WayPoints[current_index]  
-	
-		velocity = Vector2.ZERO
-		$Timer.start(wait_time)
-		is_waiting = true
-	
+
 	if direction.x > 0:
 		sprite2D.flip_h = false
 	elif direction.x < 0:
 		sprite2D.flip_h = true
+
+	makePath()
+
+
+
+func makePath() -> void:
+	if agent.is_navigation_finished():
+		current_index += 1
+
+		if current_index >= WayPoints.size():
+			current_index = 0
+
+		target = WayPoints[current_index]
+		agent.target_position = target.global_position
+		agent.set_target_position(target.global_position)
+
+		velocity = Vector2.ZERO
+		$Timer.start(wait_time)
+		is_waiting = true
+
 
 func animation(direction):
 	if is_waiting:
