c6078bdc1a84d985c81fba3de88b5a47fdcb26f6
Joaquin <joaquin.salas.dev@gmail.com>
2026-08-05 18:50:18 +0200

    feat(signal-bus): new signals for player entered and exited camera zones.

--- fabulas/autoloads/signal_bus.gd
+++ fabulas/autoloads/signal_bus.gd
@@ -1,6 +1,6 @@
 extends Node
 
-## SignalBus is a singleton that acts as an event bus for the game, allowing different parts of the game to communicate with each other directly through signals.
+## Singleton that acts as an event bus for the game, allowing different parts of the game to communicate with each other directly through signals.
 
 # ====================== PLAYER SIGNALS ======================
 ## Signal emiited when player stablishes his max health. The max health value is passed as an argument.
@@ -14,3 +14,12 @@ signal player_health_changed(new_health: int)
 ## Signal emitted when player dies.
 @warning_ignore("unused_signal")
 signal player_died()
+
+# ====================== CAMERA ZONE SIGNALS ======================
+## Signal emitted when player enters a camera zone.
+@warning_ignore("unused_signal")
+signal player_entered_camera_zone(zone: CameraZone)
+
+## Signal emitted when player exits a camera zone.
+@warning_ignore("unused_signal")
+signal player_exited_camera_zone(zone: CameraZone)
