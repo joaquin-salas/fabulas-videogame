dc68a5177ed2f1870ed4e5b238e7b27f9df4abff
isaacvc53 <isaacvc42@gmail.com>
2026-08-15 12:47:01 +0200

    Feat/maze_level : New scenes for maze level, new system of key and doors, new sounds assets.

--- fabulas/autoloads/signal_bus.gd
+++ fabulas/autoloads/signal_bus.gd
@@ -23,3 +23,7 @@ signal player_entered_camera_zone(zone: CameraZone)
 ## Signal emitted when player exits a camera zone.
 @warning_ignore("unused_signal")
 signal player_exited_camera_zone(zone: CameraZone)
+
+# ====================== KEY SIGNALS ======================
+@warning_ignore("unused_signal")
+signal bluekeytake
---
+++ fabulas/worlds/level_maze/level_maze.gd
@@ -0,0 +1,32 @@
+extends Node2D
+var whitekey : bool = false
+var bluekey : bool = true
+var redkey : bool = false
+
+@onready var blue_key: Node2D = $BlueKey
+@onready var white_key: Node2D = $WhiteKey
+@onready var bluedoor: TileMapLayer = $BluePart/Bluedoor
+
+@onready var black_rect: Node = $BluePart/BlackRect
+
+
+
+
+func _ready() -> void:
+	SignalBus.bluekeytake.connect(_on_blue_key_taken)
+
+func _on_blue_key_taken() -> void:
+	bluekey = true
+	print(bluekey)
+	SoundManager.play_sfx("Key")
+
+
+func _on_door_blue_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and bluekey == true:
+		bluedoor.clear()
+		SoundManager.play_sfx("Door")
+		bluekey = false
+	
+func _on_change_part_body_entered(body: Node2D) -> void:
+	$BluePart/ChangePart.set_deferred("monitoring", false)
+	black_rect.queue_free()
