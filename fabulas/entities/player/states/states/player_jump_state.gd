d575eeead1431d83896899475f2e596894c3bf6a
joaquin-salas <joaquin.salas.dev@gmail.com>
2026-09-21 17:11:13 +0200

    docs: improved documentation & added print debug function.

--- fabulas/entities/player/states/states/player_jump_state.gd
+++ fabulas/entities/player/states/states/player_jump_state.gd
@@ -12,10 +12,11 @@ func start() -> void:
 	player.jump_buffer_timer.stop()
 	
 	player.velocity.y = player.player_movement_stats.jump_force + player.boost_velocity.y
-	print("Jump State:")
-	print("jump_force -> ", player.player_movement_stats.jump_force)
-	print("boost_velocity -> ", player.boost_velocity)
-	print("player.velocity.y -> ", player.velocity.y)
+
+	# _debug_jump_state()
+	
+	##### This could be needed to consume the boost velocity and avoid bugs in the future #####
+	# player.boost_velocity = Vector2.ZERO
 
 func end() -> void:
 	has_cut_jump = false
@@ -32,3 +33,16 @@ func on_physics_process(delta: float) -> void:
 		state_machine.change_state(PlayerStatesNames.FALLING)
 	
 	super.on_physics_process(delta)
+
+# ******************* DEBUG FUNCTIONS *******************
+func _debug_jump_state() -> void:
+	var debug_data: Array = [
+		"=== JUMP STATE EXECUTED ===",
+		"Jump Force Base: %s" % player.player_movement_stats.jump_force,
+		"Boost Velocity Y: %s" % player.boost_velocity.y,
+		"Final Velocity Y: %s" % player.velocity.y,
+		"Is Cut Jump Active: %s" % has_cut_jump,
+		"==========================="
+	]
+	
+	player.print_debug(debug_data)
