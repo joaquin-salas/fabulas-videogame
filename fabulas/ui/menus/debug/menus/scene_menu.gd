579184cb3877fa3bfacdad6473f665786bb60b6d
isaacvc53 <isaacvc42@gmail.com>
2026-09-25 13:55:06 +0200

    feat: add debug teleport destinations
    - Add debug teleport support using Marker2D destinations
    - Allow debug menu to teleport to Level1, Level2, Level3, etc.
    - Position the player at the selected teleport marker

--- fabulas/ui/menus/debug/menus/scene_menu.gd
+++ fabulas/ui/menus/debug/menus/scene_menu.gd
@@ -1,8 +1,19 @@
 extends "../base_menu/base_menu.gd"
+
 func on_accept():
 	var option = options[selected_index]
-	var scene_id = SceneManager.SceneID.get(option.option_id, null)
-	if scene_id == null:
-		return
-	DebugMenu.toggle() 
-	SceneManager.goto(scene_id)
+
+	match option.option_id:
+		"LEVEL_START":
+			DebugMenu.toggle()
+			SceneManager.goto(SceneManager.SceneID.LEVEL_0_START)
+
+		"LEVEL_1", "LEVEL_2", "LEVEL_3","LEVEL_4","LEVEL_5":
+			var marker_name = option.option_id.replace("LEVEL_", "Level")
+
+			DebugMenu.toggle()
+			SceneManager.goto(
+				SceneManager.SceneID.LEVEL_COMPLETE,
+				marker_name
+			)
+	
--- fabulas/worlds/level_complete/level_complete.gd
+++ fabulas/worlds/level_complete/level_complete.gd
@@ -1,16 +1,40 @@
 extends Node2D
 
 @onready var keys_hud: CanvasLayer = $Menus/KeysHud
+@onready var camera_rig: Node2D = $CameraRig
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-		SoundManager.stop_music()
-		TransitionsScreen.fade_in()
-		SignalBus.hud_off.connect(_on_hud_off)
-		SignalBus.hud_on.connect(_on_hud_on)
+	SoundManager.stop_music()
+	TransitionsScreen.fade_in()
 
-func _on_hud_off() :
+	SignalBus.hud_off.connect(_on_hud_off)
+	SignalBus.hud_on.connect(_on_hud_on)
+
+	_teleport_player()
+
+
+func _teleport_player() -> void:
+	if SceneManager.teleport_marker == "":
+		return
+
+	var marker = $DebugTeleport.get_node_or_null(
+		SceneManager.teleport_marker
+	)
+
+	if marker == null:
+		return
+
+	var player = get_tree().get_first_node_in_group("player")
+
+	if player:
+		player.global_position = marker.global_position
+		camera_rig.global_position = marker.global_position
+
+
+func _on_hud_off() -> void:
 	keys_hud.visible = false
-	
-func _on_hud_on() :
+
+
+func _on_hud_on() -> void:
 	keys_hud.visible = true
