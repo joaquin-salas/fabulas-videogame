bc33a0436987b078cec2708d5e65514d4c728523
Isaac <isaacvc42@gmail.com>
2026-09-26 13:20:11 +0200

    feat: add debug teleport destinations (#46)
    - Add debug teleport support using Marker2D destinations
    - Allow debug menu to teleport to Level1, Level2, Level3, etc.
    - Position the player at the selected teleport marker

--- fabulas/shared/scene_manager/scene_manager.gd
+++ fabulas/shared/scene_manager/scene_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var current_scene_id: SceneID
+var teleport_marker: String = ""
 
 enum SceneID {
 	MAIN_MENU,
@@ -17,36 +18,43 @@ enum SceneID {
 const SCENES := {
 	SceneID.MAIN_MENU: "res://ui/menus/main_menu/main_menu.tscn",
 	SceneID.INTRO_CUTSCENE: "res://worlds/scenes/cutscenes/intro/cutscene_intro1.tscn",
-	SceneID.LEVEL_ISAAC:"res://worlds/Level_isaac/Level_isaac.tscn",
+	SceneID.LEVEL_ISAAC: "res://worlds/Level_isaac/Level_isaac.tscn",
 	SceneID.LEVEL_JOA: "res://worlds/level_test/level_test.tscn",
 	SceneID.LEVEL_0_START: "res://worlds/level_0_start/level_0_start.tscn",
 	SceneID.LEVEL_1_INTRODUCTION: "res://worlds/level_1_introduction/level_1_introduction.tscn",
-	SceneID.LEVEL_2_MOVING_PLATFORMS:"res://worlds/level_2_moving_platforms/level_2_moving_platforms.tscn",
+	SceneID.LEVEL_2_MOVING_PLATFORMS: "res://worlds/level_2_moving_platforms/level_2_moving_platforms.tscn",
 	SceneID.LEVEL_3_MAZE: "res://worlds/Level_3_maze/Level_3_maze.tscn",
 	SceneID.LEVEL_COMPLETE: "res://worlds/level_complete/level_complete.tscn"
-
-	
-	
-	
 }
 
-func goto(scene_id: SceneID) -> void:
+func goto(scene_id: SceneID, marker_name: String = "") -> void:
 	current_scene_id = scene_id
-	get_tree().call_deferred("change_scene_to_file", SCENES[scene_id])
+	teleport_marker = marker_name
+
+	get_tree().call_deferred(
+		"change_scene_to_file",
+		SCENES[scene_id]
+	)
 
 
 func paused_game(paused: bool) -> void:
 	get_tree().paused = paused
+
 	var canvas := get_tree().current_scene.get_node_or_null("Menus")
 	if canvas == null:
 		return
+
 	var pause_menu := canvas.get_node_or_null("PauseMenu")
+
 	if pause_menu:
 		pause_menu.visible = paused
 
+
 func get_current_scene_id() -> SceneID:
 	var current_path := get_tree().current_scene.scene_file_path
+
 	for id in SCENES:
 		if SCENES[id] == current_path:
-			return id	
-	return current_scene_id 
+			return id
+
+	return current_scene_id
